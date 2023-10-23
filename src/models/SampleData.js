8c4311df5ad0c3e90553ad3fc7659b864a8eb6ed
codespace-jake <jake@codespace.im>
2023-10-23 22:29:31 +0900

    feat. Schema 추가
    - 상품 데이터의 스키마 추가

---
+++ src/models/SampleData.js
@@ -0,0 +1,33 @@
+import mongoose from "mongoose";
+
+const { Schema } = mongoose;
+
+/**
+ * @typedef {Object} ProductSchema
+ * @property {string} thumbnail - 썸네일 이미지 URL
+ * @property {string} category - 카테고리
+ * @property {string} brand - 브랜드
+ * @property {string} name - 상품 제목
+ * @property {number} price - 원래 가격
+ * @property {number} saleRatio - 할인률
+//  * @property {string} inOnly - '오직 여기에서만 살 수있는 단독 상품'인가의 유무 , 추가예정
+ * @property {mongoose.Types.ObjectId} userID - 사용자 ID 참조 (User 모델과 연결된 ObjectId)
+ * @property {mongoose.Types.ObjectId[]} userIDs - 사용자 ID 배열 참조 (User 모델과 연결된 ObjectId 배열)
+ */
+
+const sampleDataSchema = new Schema(
+  {
+    thumbnail: String,
+    category: String,
+    brand: String,
+    name: String,
+    price: Number,
+    saleRatio: Number,
+    // inOnly: String,
+  },
+  { timestamps: true }
+);
+
+const 제품모델 = mongoose.model("Product_jake", sampleDataSchema);
+
+export default 제품모델;
