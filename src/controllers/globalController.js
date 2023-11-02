2a7616ce507d92bf13e2a9786aeb1938a63f0558
codespace-jake <jake@codespace.im>
2023-11-02 18:48:09 +0900

    상품페이지. 퍼블리싱 및 정렬 기능 추가
    - 상품페이지의 레이아웃 구성
    - 정렬 기능 구현
       - 가격높은순
       - 가격낮은순
       - 최신순
       - 이름순

--- src/controllers/globalController.js
+++ src/controllers/globalController.js
@@ -1,4 +1,6 @@
 import routes from "../routes";
+import paginate from "express-paginate";
+import Product from "../models/Product";
 
 // 홈 Home
 export const home = async (req, res) => {
@@ -18,5 +20,65 @@ export const anotherController = () => {};
 // 상품 Product
 
 export const product = async (req, res) => {
-  res.render("product");
+  try {
+    const {
+      query: { sort, limit },
+    } = req;
+
+    // findQuery default 값
+    const findQuery = {};
+    // sortQuery default 값
+    let sortQuery = { createdAt: -1 };
+
+    // BEGIN: 분류 기능이 있을 경우
+    const sortArr = [
+      { code: "0", title: "가격 높은 순", value: "salePrice", order: 1 },
+      { code: "1", title: "가격 낮은 순", value: "salePrice", order: -1 },
+      { code: "2", title: "최신순", value: "createdAt", order: -1 },
+      { code: "3", title: "이름순", value: "name", order: 1 },
+    ];
+
+    const sortCode = sort || sortArr[0].code;
+    if (sort) {
+      sortQuery = {};
+      sortArr.forEach((x, i) => {
+        if (x.code === sort)
+          sortQuery[`${sortArr[i].value}`] = sortArr[i].order;
+      });
+    }
+
+    // END: 분류 기능이 있을 경우
+
+    // BEGIN: pagination 데이터
+    const [adminItems, totalCount] = await Promise.all([
+      Product.find(findQuery)
+        .sort(sortQuery)
+        .limit(limit)
+        .skip(req.skip)
+        .exec(),
+      Product.countDocuments(findQuery),
+    ]);
+
+    // const pageCount = Math.ceil(totalCount / limit);
+    // const pages = paginate.getArrayPages(req)(10, pageCount, req.query.page);
+    // END: pagination 데이터
+
+    res.render("product", {
+      adminNameKo: "상품 데이터",
+      adminLink: routes.adminProduct,
+      adminItems,
+      sortArr,
+      sortCode,
+      // totalCount,
+      // pageCount,
+      // pages,
+      // limit,
+    });
+  } catch (err) {
+    console.log(err);
+    res.send(
+      `<script>alert("오류가 발생했습니다:\\r\\n${err}");\
+        location.href="${routes.home}"</script>`
+    );
+  }
 };
