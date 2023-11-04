4752a05fc933f1189a7201d50e76d4f9adb20d17
codespace-jake <jake@codespace.im>
2023-11-04 19:33:17 +0900

    공통.로그인,회원가입,로그아웃 구현
    - 로그인,로그아웃,회원가입 기능 구현
    - 레이아웃 구성 완료
    - todo. 소셜로그인 기능 구현

--- src/controllers/globalController.js
+++ src/controllers/globalController.js
@@ -1,11 +1,19 @@
 import routes from "../routes";
 import paginate from "express-paginate";
 import Product from "../models/Product";
+import passport from "passport";
+import User from "../models/User";
+import moment from "moment-timezone";
 
 // 홈 Home
-export const home = async (req, res) => {
+export const getHome = async (req, res) => {
   try {
-    res.render("home");
+    let isLogin = false;
+    if (req.user) {
+      isLogin = true;
+    }
+
+    res.render("home", { isLogin });
   } catch (err) {
     console.log(err);
     res.send(
@@ -15,12 +23,60 @@ export const home = async (req, res) => {
   }
 };
 
-export const anotherController = () => {};
+export const postHome = async (req, res, next) => {
+  try {
+    passport.authenticate("local", (err, user) => {
+      if (err) {
+        return next(err);
+      }
+      if (!user) {
+        res.send(
+          `<script>alert("로그인 정보가 잘못되었습니다.");\
+          location.href="${routes.home}"</script>`
+        );
+      } else {
+        req.logIn(user, (e) => {
+          if (err) {
+            next(e);
+          }
+          res.send(`<script>location.href="${routes.home}"</script>`);
+        });
+      }
+    })(req, res, next);
+  } catch (err) {
+    console.log(err);
+    res.send(
+      `<script>alert("오류가 발생했습니다:\\r\\n${err}");\
+      location.href="${routes.home}"</script>`
+    );
+  }
+};
+
+// 로그아웃
+export const getLogout = (req, res) => {
+  try {
+    req.logout();
+    req.session.destroy(() => {
+      res.send(`<script>location.href="${routes.home}"</script>`);
+    });
+  } catch (err) {
+    console.log(err);
+    res.send(
+      `<script>alert("오류가 발생했습니다:\\r\\n${err}");\
+      location.href="${routes.home}"</script>`
+    );
+  }
+};
 
 // 상품 Product
 
 export const product = async (req, res) => {
   try {
+    let isLogin = false;
+    if (req.user) {
+      isLogin = true;
+    }
+
     const {
       query: { sort, limit },
     } = req;
@@ -73,12 +129,121 @@ export const product = async (req, res) => {
       pageCount,
       pages,
       limit,
+      isLogin,
     });
   } catch (err) {
     console.log(err);
     res.send(
       `<script>alert("오류가 발생했습니다:\\r\\n${err}");\
         location.href="${routes.home}"</script>`
+    );
+  }
+};
+
+// 로그인
+export const getLogin = (req, res) => {
+  try {
+    if (req.user) {
+      res.send(`<script>location.href="${routes.home}"</script>`);
+    } else {
+      res.render("sign/login");
+    }
+  } catch (err) {
+    console.log(err);
+    res.send(
+      `<script>alert("오류가 발생했습니다:\\r\\n${err}");\
+      location.href="${routes.home}"</script>`
+    );
+  }
+};
+
+export const postLogin = async (req, res, next) => {
+  try {
+    passport.authenticate("local", (err, user) => {
+      if (err) {
+        return next(err);
+      }
+      if (!user) {
+        res.send(
+          `<script>alert("로그인 정보가 잘못되었습니다.");\
+          location.href="${routes.home}"</script>`
+        );
+      } else {
+        req.logIn(user, (e) => {
+          if (err) {
+            next(e);
+          }
+          res.send(`<script>location.href="${routes.home}"</script>`);
+        });
+      }
+    })(req, res, next);
+  } catch (err) {
+    console.log(err);
+    res.send(
+      `<script>alert("오류가 발생했습니다:\\r\\n${err}");\
+      location.href="${routes.home}"</script>`
+    );
+  }
+};
+
+// 회원가입
+export const getAdminRegister = (req, res) => {
+  try {
+    if (req.user) {
+      res.send(`<script>location.href="${routes.home}"</script>`);
+    } else {
+      res.render("sign/register");
+    }
+  } catch (err) {
+    console.log(err);
+    res.send(
+      `<script>alert("오류가 발생했습니다:\\r\\n${err}");\
+      location.href="${routes.home}"</script>`
+    );
+  }
+};
+export const postAdminRegister = async (req, res) => {
+  try {
+    const { body } = req;
+    const users = await User.findOne({ userID: body.userID });
+    body.role = "normal";
+    body.name = "일반 회원";
+    if (body.password !== body.password2) {
+      res.send(
+        `<script>\
+          alert("비밀번호가 일치하지 않습니다.");\
+          location.href="${routes.admin}"\
+        </script>`
+      );
+    } else if (users) {
+      res.send(
+        `<script>alert("이미 가입된 아이디 입니다.");history.go(-1);</script>`
+      );
+    } else {
+      try {
+        body.createdAt = moment(new Date()).tz("Asia/Seoul");
+        body.updatedAt = moment(new Date()).tz("Asia/Seoul");
+        const user = await User(body);
+        await User.register(user, body.password);
+        res.send(
+          `<script>\
+            alert("회원가입이 완료되었습니다.\\r\\n코드스페이스에 오신걸 환영해요!.");\
+            location.href="${routes.home}"\
+          </script>`
+        );
+      } catch (err) {
+        console.log(err);
+        res.send(
+          `<script>alert("오류가 발생했습니다:\\r\\n${err}");\
+          location.href="${routes.adminRegister}"</script>`
+        );
+      }
+    }
+  } catch (err) {
+    console.log(err);
+    res.send(
+      `<script>alert("오류가 발생했습니다:\\r\\n${err}"); \
+      location.href="${routes.adminRegister}"</script>`
     );
   }
 };
--- src/routers/globalRouter.js
+++ src/routers/globalRouter.js
@@ -1,12 +1,34 @@
 import paginate from "express-paginate";
 import express from "express";
 import routes from "../routes";
-import { home, product } from "../controllers/globalController";
+import {
+  getHome,
+  postHome,
+  product,
+  getLogin,
+  postLogin,
+  getLogout,
+  getAdminRegister,
+  postAdminRegister,
+} from "../controllers/globalController";
 
 const globalRouter = express.Router();
 
 // 홈 Home
-globalRouter.get(routes.home, home);
+globalRouter.get(routes.home, getHome);
+// globalRouter.post(routes.home, postHome);
+
+// 상품 Product
 globalRouter.get(routes.adminProduct, paginate.middleware(20, 50), product);
 
+// 로그인
+globalRouter.get(routes.login, getLogin);
+globalRouter.post(routes.login, postLogin);
+
+//로그아웃
+globalRouter.get(routes.logout, getLogout);
+
+// 회원가입
+globalRouter.get(routes.adminRegister, getAdminRegister);
+globalRouter.post(routes.adminRegister, postAdminRegister);
 export default globalRouter;
